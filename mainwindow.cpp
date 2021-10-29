4f1d35fe6ab48eb1f6a772bcf6cf525ba83962f5
PabloSerantes <pabloserantes18@gmail.com>
2021-10-29 14:19:19 -0300

    29/10/2021 14:19 1111

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,7 +14,7 @@ MainWindow::MainWindow(QWidget *parent)
     QSerialPort1 -> setPortName("COM3");
     QSerialPort1 -> setBaudRate(115200);
     QSerialPort1 -> setDataBits(QSerialPort :: Data8);
-    QSerialPort1 -> setParity(QSerialPort :: NoParity); // Para st no parity
+    QSerialPort1 -> setParity(QSerialPort :: NoParity);
     QSerialPort1 -> setFlowControl(QSerialPort :: NoFlowControl);
 
     connect(QSerialPort1, &QSerialPort::readyRead, this, &MainWindow::onQSerialPort1Rx);
@@ -47,12 +47,40 @@ void MainWindow::onQTimer1(){
     if(QSerialPort1 -> isOpen()){
         switch(gameState){
             case STARTING:
-            break;
-
-            case IDLE:
-                command = SETLEDSTATE;
-                payloadTx[0] = LED1;
+                commandTx = GETLEDSTATE;
                 onQSerialPort1Tx();
+                if(commandRx == GETLEDSTATE){
+                    for(int i = 0; i < 4; i++){
+                        if(leds[i] == '0'){
+                            ledDrawer(false, i);
+                        }else if(leds[i] == '1'){
+                            ledDrawer(true, i);
+                        }
+                    }
+                }
+                commandTx = GETBUTTONSTATE;
+                onQSerialPort1Tx();
+                if(commandRx == GETBUTTONSTATE){
+                    for(int i = 0; i < 4; i++){
+                        if(buttons[i] == '0'){
+                            buttonDrawer(false, i);
+                        }else if(buttons[i] == '1'){
+                            buttonDrawer(true, i);
+                        }
+                    }
+                }
+                gameState = IDLE;
+            break;
+            case IDLE:
+                if(commandRx == GETBUTTONSTATE){
+                    for(int i = 0; i < 4; i++){
+                        if(buttons[i] == '0'){
+                            buttonDrawer(false, i);
+                        }else if(buttons[i] == '1'){
+                            buttonDrawer(true, i);
+                        }
+                    }
+                }
             break;
 
             case GAMEON:
@@ -62,13 +90,18 @@ void MainWindow::onQTimer1(){
             break;
         }
     }
+
     if(header){
         timeoutRx--;
         if(!timeoutRx){
             header = 0;
         }
     }
-    //--------------------------------  LEDS  -----------------------------------------------//
+}
+
+
+
+void MainWindow::ledDrawer(bool onoff, int led){
     QBrush brush;
     QPainter paint(QPaintBox1 -> getCanvas());
     int h = QPaintBox1 -> height();
@@ -76,88 +109,125 @@ void MainWindow::onQTimer1(){
     int r = h/2;
     int xDrawPos;
 
-    if(command == GETLEDSTATE || command == SETLEDSTATE){
-        xDrawPos = -150;
-        for(int i = 0; i < 4; i++){
-            if((payloadRx.mid(i, 1)) == '0'){
-                brush.setColor(Qt::white);
-                brush.setStyle(Qt::SolidPattern);
-                paint.setBrush(brush);
-                paint.drawEllipse(((w/2) - r + xDrawPos), r - (r / 1.7) - 30, (2 * r) - 90, (2 * r) - 90);
-                xDrawPos += 129;
-            }
-            if((payloadRx.mid(i, 1)) == '1'){
-                brush.setColor(Qt::red);
-                brush.setStyle(Qt::SolidPattern);
-                paint.setBrush(brush);
-                paint.drawEllipse(((w/2) - r + xDrawPos), r - (r / 1.7) - 30, (2 * r) - 90, (2 * r) - 90);
-                xDrawPos += 129;
-            }
-        }
-    }
-    //----------------------------------------------------------------------------------------//
+    switch(led){
+        case LED1:
+            xDrawPos = -150;
+        break;
 
-    //------------------------------------  BOTONES  ---------------------------------------//
-    if(command == GETBUTTONSTATE){
-        xDrawPos = -105;
-        for(int i = 0;  i < 4; i++){
-            if(payloadRx.mid(i,1) == '0'){
-                brush.setColor(Qt::gray);
-                brush.setStyle(Qt::SolidPattern);
-                paint.setBrush(brush);
-                paint.drawEllipse(((w/2) - r + xDrawPos), r - (r / 1.7) + 90, (2 * r) - 180, (2 * r) - 180);
-                xDrawPos += 129;
-            }
-            if(payloadRx.mid(i,1) == '1'){
-                brush.setColor(Qt::blue);
-                brush.setStyle(Qt::SolidPattern);
-                paint.setBrush(brush);
-                paint.drawEllipse(((w/2) - r + xDrawPos), r - (r / 1.7) + 90, (2 * r) - 180, (2 * r) - 180);
-                xDrawPos += 129;
-            }
-        }
+        case LED2:
+            xDrawPos = -21;
+        break;
+
+        case LED3:
+            xDrawPos = 108;
+        break;
+
+        case LED4:
+            xDrawPos = 237;
+        break;
+    }
+
+    if(!onoff){
+        brush.setColor(Qt::white);
+        brush.setStyle(Qt::SolidPattern);
+        paint.setBrush(brush);
+        paint.drawEllipse((w/2) - r + xDrawPos, r - (r / 1.7) - 30, (2 * r) - 90, (2 * r) - 90);
+    }
+    if(onoff){
+        brush.setColor(Qt::red);
+        brush.setStyle(Qt::SolidPattern);
+        paint.setBrush(brush);
+        paint.drawEllipse((w/2) - r + xDrawPos, r - (r / 1.7) - 30, (2 * r) - 90, (2 * r) - 90);
     }
     QPaintBox1 -> update();
 }
 
-void MainWindow::onQSerialPort1Tx(){
-        uint8_t tx[12];
-        if(QSerialPort1 -> isOpen()){
-            tx[0] = 'U';
-            tx[1] = 'N';
-            tx[2] = 'E';
-            tx[3] = 'R';
 
-            tx[5] = ':';
-            tx[6] = command;
-            if(command == ALIVE || command == GETLEDSTATE){
-                tx[7] = 0;
-                for(int i = 0; i < 7; i++){
-                    tx[7] ^= tx[i];                    
-                }
-                tx[4] = 2;
-                //potato.jpg
-                QMessageBox::information(this, "PORT", "Conexion exitosa!", "OK");
-                QSerialPort1 -> write((char *)tx, 8);
-                if(gameState == STARTING){
-                    gameState = IDLE;
-                }
-            }
-            if(command == SETLEDSTATE){
-                tx[8] = 0;
-                tx[7] = payloadTx[0];
-                for(int i = 0; i < 8; i++){
-                    tx[8] ^= tx[i];
-                }
-                tx[4] = 3;
-                QMessageBox::information(this, "PORT", "Conexion exitosa!", "OK");
-                QSerialPort1 -> write((char *)tx, 9);
-                gameState = GAMEON;
-            }
-        }else{
-            QMessageBox::information(this, "PORT", "El dispositivo no esta conectado");
-        }
+void MainWindow::buttonDrawer(bool onoff, int button){
+    QBrush brush;
+    QPainter paint(QPaintBox1 -> getCanvas());
+    int h = QPaintBox1 -> height();
+    int w = QPaintBox1 -> width();
+    int r = h/2;
+    int xDrawPos;
+
+    switch(button){
+        case BUTTON1:
+            xDrawPos = -105;
+        break;
+
+        case BUTTON2:
+            xDrawPos = 24;
+        break;
+
+        case BUTTON3:
+            xDrawPos = 153;
+        break;
+
+        case BUTTON4:
+            xDrawPos = 282;
+        break;
+    }
+    if(!onoff){
+        brush.setColor(Qt::gray);
+        brush.setStyle(Qt::SolidPattern);
+        paint.setBrush(brush);
+        paint.drawEllipse(((w/2) - r + xDrawPos), r - (r / 1.7) + 90, (2 * r) - 180, (2 * r) - 180);
+    }
+    if(onoff){
+        brush.setColor(Qt::blue);
+        brush.setStyle(Qt::SolidPattern);
+        paint.setBrush(brush);
+        paint.drawEllipse(((w/2) - r + xDrawPos), r - (r / 1.7) + 90, (2 * r) - 180, (2 * r) - 180);
+    }
+    QPaintBox1 -> update();
 }
+
+
+void MainWindow::onQSerialPort1Tx(){
+    uint8_t tx[12];
+    if(QSerialPort1 -> isOpen()){
+        tx[0] = 'U';
+        tx[1] = 'N';
+        tx[2] = 'E';
+        tx[3] = 'R';
+        tx[5] = ':';
+        tx[6] = commandTx;
+        if(commandTx == ALIVE || commandTx == GETLEDSTATE || commandTx == GETBUTTONSTATE){
+            tx[4] = 2;
+            tx[7] = 0;
+            for(int i = 0; i < 7; i++){
+                tx[7] ^= tx[i];
+            }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+           /* for(int i = 0; i < 8; i++){
+                strTx = strTx + QString("%1").arg(tx[i], 2, 16, QChar('0')).toUpper();
+            }
+            ui->lineEdit->setText(strTx);
+            strTx = "";*/
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+            //potato.jpg
+            //QMessageBox::information(this, "PORT", "Conexion exitosa!", "OK");
+            QSerialPort1 -> write((char *)tx, 8);
+        }
+        if(commandTx == SETLEDSTATE){
+            tx[4] = 3;
+            tx[8] = 0;
+            tx[7] = payloadTx[0];
+            for(int i = 0; i < 8; i++){
+                tx[8] ^= tx[i];
+            }
+
+            //QMessageBox::information(this, "PORT", "Conexion exitosa!", "OK");
+            QSerialPort1 -> write((char *)tx, 9);
+        }
+    }else{
+        QMessageBox::information(this, "PORT", "El dispositivo no esta conectado");
+    }
+}
+
 
 void MainWindow::onQSerialPort1Rx(){
     int count;
@@ -173,7 +243,6 @@ void MainWindow::onQSerialPort1Rx(){
     for (int i = 0; i < count; i++) {
         strRx = strRx + QString("%1").arg(buf[i], 2, 16, QChar('0')).toUpper();
     }
-
 
     for(int i=0; i < count; i++){
         switch (header) {
@@ -224,12 +293,13 @@ void MainWindow::onQSerialPort1Rx(){
             case 6: //ID + PAYLOAD + CKS
                 bufRx[index++] = buf[i];
 
+                //Identifico el comando recibido
                 IDRx = strRx.mid(12);
                 IDRx = IDRx.mid(0, 2);
-                command = IDRx.toUInt(&ok, 16);
 
-                payloadRx = QString("%1").arg(strRx.mid(14, strRx.length() - 18).toULongLong(&ok, 16), 4, 2, QChar('0'));
 
+                //Separo la cadena del payload de la cadena entera
+                payloadRx = QString("%1").arg(strRx.midRef(14, strRx.length() - 18).toULongLong(&ok, 16), 4, 2, QChar('0'));
 
                 if(nbytes != 1){
                     cks ^= buf[i];
@@ -238,14 +308,44 @@ void MainWindow::onQSerialPort1Rx(){
                 if(!nbytes){
                     header = 6;
                     if(buf[i] == cks){
+                        recieved = true;
                         ui->lineEdit->clear();
                         ui->lineEdit->setText(strRx);
                         strRx = "";
+                    }else{
+                        recieved = false;
                     }
                 }
             break;
             default:
                 header = 0;
+        }
+    }
+
+    if(recieved){
+        //Decodifico el comando recibido
+        commandRx = IDRx.toUInt(&ok, 16);
+
+        switch(commandRx){
+            case ALIVE:
+
+            break;
+            case GETLEDSTATE:
+                for(int i = 0; i < 4; i++){
+                    leds[i] = payloadRx.mid(i, 1);
+                }
+            break;
+            case SETLEDSTATE:
+
+            break;
+            case BUTTONEVENT:
+
+            break;
+            case GETBUTTONSTATE:
+                for(int i = 0; i < 4; i++){
+                    buttons[i] = payloadRx.mid(i, 1);
+                }
+            break;
         }
     }
     delete[] buf;
@@ -255,9 +355,8 @@ void MainWindow::on_radioButton_toggled(bool checked){
 
     if(checked){
         if(QSerialPort1 -> open(QSerialPort::ReadWrite)){
-            command = ALIVE;
+            commandTx = ALIVE;
             onQSerialPort1Tx();
-       //   gameState = IDLE;
         }else{
             ui -> radioButton_2 -> setChecked(true);
             QMessageBox::information(this, "PORT", "No se pudo abrir el puerto");
--- mainwindow.h
+++ mainwindow.h
@@ -8,6 +8,7 @@
 #include <qpaintbox.h>
 #include <stdlib.h>
 
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -25,6 +26,8 @@ private slots:
     void onQSerialPort1Tx();
     void onQTimer1();
     void on_radioButton_toggled(bool checked);
+    void ledDrawer(bool onoff, int led);
+    void buttonDrawer(bool onoff, int button);
 
 private:
     typedef enum{
@@ -33,13 +36,15 @@ private:
         GAMEON,
         LOST,
     }_eGameState;
+
     Ui::MainWindow *ui;
     QSerialPort *QSerialPort1;
-    QString strRx, IDRx, payloadRx, strTx;
-    uint8_t bufRx[48], index, nbytes, cks, header, timeoutRx, command, gameState = IDLE, payloadTx[48];
+    QString strRx, payloadRx, strTx, IDRx, leds[4], buttons[4];
+    uint8_t bufRx[48], index, nbytes, cks, header, timeoutRx, commandTx, commandRx, gameState = IDLE, payloadTx[48];
     uint32_t puntaje;
     QTimer *QTimer1;
     QPaintBox *QPaintBox1;
+    bool recieved;
 
     typedef enum{
            ACK=0x0D,
@@ -56,5 +61,13 @@ private:
         LED3,
         LED4,
     }_eLEDS;
+
+    typedef enum{
+        BUTTON1,
+        BUTTON2,
+        BUTTON3,
+        BUTTON4
+    }_eBUTTON;
+
 };
 #endif // MAINWINDOW_H
